ef873c2c54bff75f7e638617d30b159f13cc1bf4
Ahmed ramy <ahmedramy.ar29@gmail.com>
2025-01-25 00:23:21 +0200

    Update app.py

--- movie_reviews/app2/app.py
+++ movie_reviews/app2/app.py
@@ -24,9 +24,8 @@ try:
     svc = pickle.load(open('movie_reviews/app2/artifacts/svc.pkl', 'rb'))
     tf_model = pickle.load(open('movie_reviews/app2/artifacts/tf.pkl', 'rb'))
     st.success("Models loaded successfully!")
-    time.sleep(0.1)
-    success_message = st.empty()
-    success_message.empty()
+    time.sleep(0.01)
+    st.empty()
 except FileNotFoundError as e:
     st.error(f"Model file not found: {e}")
     lr, dt, svc, tf_model = None, None, None, None
