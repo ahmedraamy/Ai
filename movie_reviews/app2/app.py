db70ae7eb37d49663ff8afb061d04337ced8716a
Ahmed ramy <ahmedramy.ar29@gmail.com>
2025-01-25 00:36:09 +0200

    Update app.py

--- movie_reviews/app2/app.py
+++ movie_reviews/app2/app.py
@@ -13,6 +13,7 @@ from transformers import AutoTokenizer  # Add this import for tokenizer
 
 # Download necessary NLTK resources
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
