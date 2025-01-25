b60b8a228c5c325b7e9e6eb3dda9f4c826273f50
Ahmed ramy <ahmedramy.ar29@gmail.com>
2025-01-25 17:45:06 +0200

    Update app.py

--- Face_Expression_Recognition/app.py
+++ Face_Expression_Recognition/app.py
@@ -54,7 +54,7 @@ if uploaded_file is not None:
 
 st.markdown(
     """
-    <div style='text-align: center; color: #800080; font-size: 19px;'>
+    <div style='text-align: center; color: #800080; font-size: 12px;'>
         <p>Created by Ahmed ramy</p>
     </div>
 """,
